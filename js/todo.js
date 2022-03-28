a6547b30d63836f6976b4a5365c6ac7c9297ea49
rachel <borapark13@gmail.com>
2022-03-28 23:23:54 +0900

    로컬스토리지에 리스트object 형태로 저장하기

--- js/todo.js
+++ js/todo.js
@@ -2,16 +2,24 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)); //toDos array를 local storage 안에 집어넣기
+}
+
 function removeToDoList(event) {
   //console.dir(event.target); //클릭된 html element (button태그)
   const li = event.target.parentElement; //element의 부모, 즉 클릭한 버튼의 부모요소인 li를 의미함
-  li.remove();
+  if (li.id === newTodo.id) li.remove();
 }
 
-function paintToDo(newTodoList) {
+function paintToDo(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const lists = document.createElement("span");
-  lists.innerText = newTodoList;
+  lists.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "x";
   button.addEventListener("click", removeToDoList);
@@ -25,10 +33,21 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  paintToDo(newTodo);
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-//todo 버튼 삭제하기
-//새로고침해도 영역이사라지지 않게 하기
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos; //toDos에 parseToDos를 넣어서 원래 있던 toDos들이 없어지지 않도록 하기
+  parsedToDos.forEach(paintToDo);
+}

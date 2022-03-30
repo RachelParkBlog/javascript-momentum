0f10b0ac72a8f38b6344b7e5b60381e7f0d84372
rachel <borapark13@gmail.com>
2022-03-30 22:40:47 +0900

    [Add]Weatehr API fetching 해보기

--- js/weather.js
+++ js/weather.js
@@ -1,4 +1,11 @@
-function onGeoOk() {}
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
 
 function onGeoErr() {
   alert("Can't find you. No Weather for you.");
